d8de1edd6964c5edc4afe12cf4f5ead1fbee9363
Azeem Sajjad <azim5289007@gmail.com>
2019-01-21 20:19:21 +0530

    todolist

---
+++ 06todolist*/app.js
@@ -0,0 +1,52 @@
+var ul = document.getElementById("list")
+var removeAll = document.getElementById("removeAll")
+var add = document.getElementById("add")
+
+add.onclick = function () {
+    addLi(ul)
+}
+
+function addLi(targetUl) {
+    var inputText = document.getElementById("text").value
+    var li = document.createElement("li")
+    var textNode = document.createTextNode(inputText + ' ')
+    var check = document.createElement("input")
+    check.type = "checkbox"
+    var editButton = document.createElement("button")
+    var removeButton = document.createElement("button")
+    document.getElementById("text").value = ""
+
+    if (inputText.length === 0) {
+        alert("Please Enter something")
+        return false
+    }
+
+    editButton.innerHTML = "Edit"
+    removeButton.innerHTML = "Delete"
+
+    li.appendChild(check)
+    li.appendChild(textNode)
+    li.appendChild(editButton)
+    li.appendChild(removeButton)
+
+    removeButton.setAttribute("onclick", "removeMe(this)")
+    editButton.setAttribute("onclick", "editMe(this)")
+
+    //problem
+    targetUl.appendChild(li)
+}
+
+function removeMe(item) {
+    var parent = item.parentElement
+    parent.parentElement.removeChild(parent)
+}
+
+function editMe(item) {
+    var parent = prompt("Please Enter New ToDos")
+    parent.parentElement.parentNode = parent
+}
+
+removeAll.onclick = function () {
+    ul.innerHTML = ""
+}
+
